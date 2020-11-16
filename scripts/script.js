d8fa0eed402cc60de3a05bea5a21d560f65277c0
phedrakeson <gabrield.developer@gmail.com>
2020-11-16 14:32:16 -0300

    scroll suave para link interno

--- scripts/script.js
+++ scripts/script.js
@@ -39,3 +39,32 @@ function initAccordion() {
 }
 
 initAccordion();
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"')
+
+function initScrollSuave() {
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+
+    // melhor alternativa
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    })
+
+    // forma alternativa
+    // const topo = section.offsetTop;
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: 'smooth',
+    // })
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  })
+}
+
+initScrollSuave();
