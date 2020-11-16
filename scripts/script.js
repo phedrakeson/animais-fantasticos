7061d875c25215e26a6f86833de212bb6dcc962b
phedrakeson <gabrield.developer@gmail.com>
2020-11-16 17:25:24 -0300

    implementando animacao no scroll

--- scripts/script.js
+++ scripts/script.js
@@ -68,3 +68,26 @@ function initScrollSuave() {
 }
 
 initScrollSuave();
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+  if(sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top - windowMetade;
+        const isSectionVisible = (sectionTop - windowMetade) < 0;
+        if(isSectionVisible) {
+          section.classList.add('scrollToggle');
+        }
+      })
+    }
+  }
+
+  animaScroll();
+
+  window.addEventListener('scroll', animaScroll);
+}
+
+initAnimacaoScroll();
