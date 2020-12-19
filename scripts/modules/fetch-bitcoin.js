64fa740847e62d8485731db5825e86a44bc4420d
phedrakeson <gabrield.developer@gmail.com>
2020-12-18 22:23:50 -0300

    adicionando consumo de api Bitcoin na seção de doação

---
+++ scripts/modules/fetch-bitcoin.js
@@ -0,0 +1,11 @@
+export default function initFetchBitcoin() {
+
+}
+
+fetch('https://blockchain.info/ticker')
+.then(r => r.json())
+.then(bitcoin => {
+  document.querySelector('.btc-preco').innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
+}).catch(erro => {
+  console.log(Error(erro));
+}) 
