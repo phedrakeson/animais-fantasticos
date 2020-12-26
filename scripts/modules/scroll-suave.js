b614b4cc68cfe65313386b9c0a51218eb014135a
phedrakeson <gabrield.developer@gmail.com>
2020-12-26 19:42:46 -0300

    scroll suave transformado em classe

--- scripts/modules/scroll-suave.js
+++ scripts/modules/scroll-suave.js
@@ -1,25 +1,34 @@
-export default function initScrollSuave() {
-  const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"')
-  function scrollToSection(event) {
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links)
+    if (options === undefined) {
+      this.options = { behavior: 'smooth', block: 'start' }
+    } else {
+      this.options = options;
+    }
+
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
+  scrollToSection(event) {
     event.preventDefault();
     const href = event.currentTarget.getAttribute('href');
     const section = document.querySelector(href);
 
-    // melhor alternativa
-    section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    })
-
-    // forma alternativa
-    // const topo = section.offsetTop;
-    // window.scrollTo({
-    //   top: topo,
-    //   behavior: 'smooth',
-    // })
+    section.scrollIntoView(this.options)
   }
 
-  linksInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection);
-  })
+  addLinkEvent() {
+    this.linksInternos.forEach((link) => {
+      link.addEventListener('click', this.scrollToSection);
+    });
+  }
+
+  init() {
+    if (this.linksInternos.length) {
+      this.addLinkEvent();
+    }
+    return this;
+  }
+
+  
 }
