2ecb6a0f6a10b31f39f48d8e335ba9ecc4729747
phedrakeson <gabrield.developer@gmail.com>
2020-12-27 20:42:55 -0300

    refatorando modal em class

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 import ScrollSuave from './modules/scroll-suave.js';
 import Accordion from './modules/accordion.js';
 import TabNav from './modules/tab-nav.js';
+import Modal from './modules/modal.js';
 import initAnimacaoScroll from './modules/scroll-animacao.js';
-import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js'
@@ -13,12 +13,14 @@ import initFetchBitcoin from './modules/fetch-bitcoin.js';
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 const accordion = new Accordion('[data-anime="accordion"] dt');
 const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
+
 scrollSuave.init();
 accordion.init();
 tabNav.init();
+modal.init();
 
 initAnimacaoScroll(); 
-initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
