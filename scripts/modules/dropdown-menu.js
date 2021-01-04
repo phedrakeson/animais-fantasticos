8f358cb1bb35b5e9e0692fd62efc648b0bd7a24e
phedrakeson <gabrield.developer@gmail.com>
2021-01-04 14:25:21 -0300

    refatorando dropdown-menu em class

--- scripts/modules/dropdown-menu.js
+++ scripts/modules/dropdown-menu.js
@@ -1,19 +1,35 @@
 import outsideClick from './outsideclick.js'
-export default function initDropdownMenu() {
-  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+export default class DropdownMenu {
+  constructor(dropdownMenus, events) {
+    this.dropdownMenus = document.querySelectorAll(dropdownMenus);
+    this.activeClass = 'active';
+    if(events === undefined) this.events = ['touchstart', 'click'];
+    else this.events = events;
+    this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
+  }
 
-  dropdownMenus.forEach(menu => {
-    ['touchstart', 'click'].forEach(userEvent => {
-      menu.addEventListener(userEvent, handleClick)
+  addDropdownMenusEvent() {
+    this.dropdownMenus.forEach(menu => {
+      this.events.forEach(userEvent => {
+        menu.addEventListener(userEvent, this.activeDropdownMenu)
+      });
     });
-  });
+  }
 
-  function handleClick(event) {
+  activeDropdownMenu(event) {
     event.preventDefault();
-    this.classList.add('active')
-    outsideClick(this, ['touchstart', 'click'], () => {
-      this.classList.remove('active');
+    const element = event.currentTarget;
+    element.classList.add(this.activeClass);
+    outsideClick(element, this.events, () => {
+      element.classList.remove(this.activeClass);
     });
+  }
+
+  init() {
+    if(this.dropdownMenus.length) {
+      this.addDropdownMenusEvent();
+    }
+    return this;
   }
 
 }
