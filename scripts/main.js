825c2f137ffa42ac37b3107d01d09e407c935891
phedrakeson <gabrield.developer@gmail.com>
2021-01-04 15:28:39 -0300

    corrigindo path da api

--- scripts/main.js
+++ scripts/main.js
@@ -32,4 +32,4 @@ funcionamento.init();
 
 
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
-fetchAnimais('../../animaisapi.json', '.numeros-grid');
+fetchAnimais('./animaisapi.json', '.numeros-grid');
